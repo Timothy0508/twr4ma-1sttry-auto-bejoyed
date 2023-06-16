027f8290dec44ea3d3014a0bd85f69c041eb1617
Timothy0508 <99249271+Timothy0508@users.noreply.github.com>
2023-06-16 12:47:05 +0800

    MakeCode 專案的初始檔案

---
+++ README.md
@@ -0,0 +1,64 @@
+
+> 在 [https://huangshixuan1.github.io/twr4ma-1sttry-auto/](https://huangshixuan1.github.io/twr4ma-1sttry-auto/) 打開此頁面
+
+## 作為擴充功能使用
+
+可以在 MakeCode 中將此儲存庫新增為**擴充功能**。
+
+* 開啟 [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* 按一下**新專案**
+* 按一下工具齒輪選單下的**擴充功能**
+* 搜索 **https://github.com/huangshixuan1/twr4ma-1sttry-auto** 並匯出
+
+## 編輯此專案 ![Build status badge](https://github.com/huangshixuan1/twr4ma-1sttry-auto/workflows/MakeCode/badge.svg)
+
+編輯 MakeCode 中的儲存庫。
+
+* 開啟 [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* 按一下**匯入**，然後按一下**匯入 URL**
+* 貼上 **https://github.com/huangshixuan1/twr4ma-1sttry-auto** 並按一下匯入
+
+## Blocks preview
+
+This image shows the blocks code from the last commit in master.
+This image may take a few minutes to refresh.
+
+![A rendered view of the blocks](https://github.com/huangshixuan1/twr4ma-1sttry-auto/raw/master/.github/makecode/blocks.png)
+
+#### 中繼資料 (用於搜索、渲染)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
+
+
+
+> 在 [https://timothy0508.github.io/twr4ma-1sttry-auto-bejoyed/](https://timothy0508.github.io/twr4ma-1sttry-auto-bejoyed/) 打開此頁面
+
+## 作為擴充功能使用
+
+可以在 MakeCode 中將此儲存庫新增為**擴充功能**。
+
+* 開啟 [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* 按一下**新專案**
+* 按一下工具齒輪選單下的**擴充功能**
+* 搜索 **https://github.com/timothy0508/twr4ma-1sttry-auto-bejoyed** 並匯出
+
+## 編輯此專案 ![Build status badge](https://github.com/timothy0508/twr4ma-1sttry-auto-bejoyed/workflows/MakeCode/badge.svg)
+
+編輯 MakeCode 中的儲存庫。
+
+* 開啟 [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* 按一下**匯入**，然後按一下**匯入 URL**
+* 貼上 **https://github.com/timothy0508/twr4ma-1sttry-auto-bejoyed** 並按一下匯入
+
+## Blocks preview
+
+This image shows the blocks code from the last commit in master.
+This image may take a few minutes to refresh.
+
+![A rendered view of the blocks](https://github.com/timothy0508/twr4ma-1sttry-auto-bejoyed/raw/master/.github/makecode/blocks.png)
+
+#### 中繼資料 (用於搜索、渲染)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,139 @@
+// 8上升
+// 2下降
+// 4向左轉
+// 6向右轉
+// 1上升C
+// 3下降E
+// 7向左轉F
+// 9向右轉D
+// 0停止
+// 
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 8) {
+        Raise()
+        basic.showLeds(`
+            . . # . .
+            . # . # .
+            # . . . #
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(50)
+        basic.clearScreen()
+    }
+    if (receivedNumber == 2) {
+        Lower()
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # . . . #
+            . # . # .
+            . . # . .
+            `)
+        basic.pause(50)
+        basic.clearScreen()
+    }
+    if (receivedNumber == 4) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # . . . .
+            . # . . .
+            . . # . .
+            `)
+        basic.pause(50)
+        basic.clearScreen()
+    }
+    if (receivedNumber == 6) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            . . . . #
+            . . . # .
+            . . # . .
+            `)
+        basic.pause(50)
+        basic.clearScreen()
+    }
+    if (receivedNumber == 1) {
+        basic.showLeds(`
+            . . # . .
+            . # . # .
+            # . . . #
+            . . . . .
+            . . # . .
+            `)
+        basic.pause(50)
+        basic.clearScreen()
+    }
+    if (receivedNumber == 3) {
+        basic.showLeds(`
+            . . # . .
+            . . . . .
+            # . . . #
+            . # . # .
+            . . # . .
+            `)
+        basic.pause(50)
+        basic.clearScreen()
+    }
+    if (receivedNumber == 7) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # . . . #
+            . . . # .
+            . . # . .
+            `)
+        basic.pause(50)
+        basic.clearScreen()
+    }
+    if (receivedNumber == 9) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # . . . #
+            . # . . .
+            . . # . .
+            `)
+        basic.pause(50)
+        basic.clearScreen()
+    }
+    if (receivedNumber == 0) {
+        stop()
+        basic.showLeds(`
+            . # # # .
+            # # . . #
+            # . # . #
+            # . . # #
+            . # # # .
+            `)
+        basic.pause(50)
+        basic.clearScreen()
+    }
+})
+function Lower () {
+    sensors.DDMmotor(
+    AnalogPin.P13,
+    0,
+    AnalogPin.P14,
+    128
+    )
+}
+function Raise () {
+    sensors.DDMmotor(
+    AnalogPin.P13,
+    1,
+    AnalogPin.P14,
+    128
+    )
+}
+function stop () {
+    sensors.DDMmotor(
+    AnalogPin.P13,
+    0,
+    AnalogPin.P14,
+    0
+    )
+}
+radio.setGroup(1)
